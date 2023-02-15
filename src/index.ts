48c65752f2897bee7f54d663df69cb83c4273fed
mohammad farhadi <m.sabah.farhadi@gmail.com>
2023-02-15 22:53:06 +0100

    add morgan for log and create first test route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello World!")
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+const PORT = Number(process.env.PORT ?? 8080);
+
+app.use(morgan("dev")); //* log with some colors :) */
+
+app.get("/", (req, res) => {
+    res.json({hello: "world"})
+});
+
+app.listen(PORT, "0.0.0.0", () => { //! 0.0.0.0 is for docker (mandatory) */
+    console.log(`Server started at http://localhost:${PORT}`)
+});
